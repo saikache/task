66dcca3ed287e9f15286dd03ea1c15139a8b90b4
saisagar <saisagar@sageabletech.com>
2016-10-05 20:04:23 +0530

    hi page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,4 +5,8 @@ class HomeController < ApplicationController
     @side_linkes_json = JSON.load(open("http://sketches.quintype.com/api/v1/config"))
     @side_linkes = @side_linkes_json['layout']['menu']#.collect{|s| [s['title'],s['section-slug']]}
   end
+
+  def my_layout
+    render :layout => 'no_layout'
+  end
 end
